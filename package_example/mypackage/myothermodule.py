958504e1b22f57e61ff5d173eeb867d5b9867e54
Chinmay <chinmay@hybrowlabs.com>
2022-10-03 22:03:46 +0530

    fix: package example

---
+++ package_example/mypackage/myothermodule.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from .mymodule import as_int
+
+# Exported function
+def add(a, b):
+    return as_int(a) + as_int(b)
+
+# Test function for module  
+def _test():
+    assert add('1', '1') == 2
+
+if __name__ == '__main__':
+    _test()
